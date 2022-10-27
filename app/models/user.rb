c24807e311003507c5e31250e7fbb638f4157a1f
Sebastian Terleira <108709097+SebastianTerleira@users.noreply.github.com>
2022-10-27 15:04:52 -0500

    Feat/user validations (#8)
    * ADD: validation-email
    
    * ADD: validation-username
    
    * ADD: validation-name
    
    * ADD: validation-avatar
    
    * ADD: validation-role
    
    * Modify(LN8) add atribute default
    
    Co-authored-by: sebastianterleira <paulotijero@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+	# Validations
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}, allow_blank: false
+	validates :username, presence: true, uniqueness: true
+	validate :name, presence: true
+	validates :avatar, presence: false
+	validates :password, presence: true, uniqueness: true, length: { maximum: 16 }
+	#   has_secure_password
+	enum role: { basic: 0,
+    admin: 1 }
 end
--- db/migrate/20221027180407_create_users.rb
+++ db/migrate/20221027180407_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :username
       t.string :name
       t.string :password
-      t.integer :role
+      t.integer :role, default: 0
 
       t.timestamps
     end
