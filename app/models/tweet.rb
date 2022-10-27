9df68fa3c9a136eb5f5ecb62b6ce67cc57e9073a
Sebastian Terleira <108709097+SebastianTerleira@users.noreply.github.com>
2022-10-27 15:24:53 -0500

    Feat/validation tweet (#9)
    * ADD: validation-body
    
    * ADD: validation replied_to
    
    * Modify: add atributes default and length in migration tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  belongs_to :replied_to, class_name: "Tweet", optional: true, counter_cache: :replies_count
+   # Validates
+   validates :body, presence: true, length: { maximum: 140 }
 end
--- db/migrate/20221027182202_create_tweets.rb
+++ db/migrate/20221027182202_create_tweets.rb
@@ -1,9 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
-      t.text :body
-      t.integer :replies_count
-      t.integer :likes_count
+      t.text :body, length: 140
+      t.integer :replies_count, default: 0
+      t.integer :likes_count, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
