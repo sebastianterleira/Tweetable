bc58a0e417ee883d3d0da58eb3e022162dc58d1e
sebastianterleira <seb.terleira1204@gmail.com>
2022-10-29 13:36:00 -0500

    Preparando los seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Seading - START"
+
+Like.destroy_all
+Tweet.destroy_all
+User.destroy_all
+
+puts "Seeding custom admin -> admin@gmail.com - supersecret"
+admin = User.create(email: "admin@gmail.com", username: "elBossAdmin", name: "admin", password: "supersecret", password_confirmation: "supersecret")
+admin.cover.attach(io: File.open('db/image/puño.png'), filename: 'puño.png')
+
