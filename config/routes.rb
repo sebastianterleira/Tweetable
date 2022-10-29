d708c64d217842b2689623d9321462bd289b6a4f
sebastianterleira <seb.terleira1204@gmail.com>
2022-10-29 11:11:05 -0500

    ADD: Views the device

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   resources :likes, only: %i[create]
   resources :tweets
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  get "/profile", to: "users#show"
 end
